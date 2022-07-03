b7a20796b980e0af9357a94e579369860a20d3ca
gfpacheco <guilherme.f.pacheco@hotmail.com>
2022-07-03 03:25:42 +0000

    Deploying to gh-pages from @ gfpacheco/general@0162339f44cd987719ef917839822656d42a152a 🚀

--- _next/static/3BNpjWuhd_3gSQiBKpu0B/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-ebad8b5e4c41d996.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
